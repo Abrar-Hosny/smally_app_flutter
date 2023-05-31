b07c2c582f87db97c27f00753a497c6725f82878
Abrar <abrarhosny19@gmail.com>
2023-05-31 10:26:33 -0700

    photos file added

---
+++ lib/Screens/photos.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/Screens/welcomepage.dart';
+
+class photos extends StatelessWidget {
+  const photos({super.key});
+  static const String id = "Photos";
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          // خلفية صورة
+
+          body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/1.jpg"), fit: BoxFit.fill)),
+        child: Column(children: [
+          //this is a buttton inside it text called Elvated Buttons
+
+          // صورة عادية assest image
+          Container(
+            child: Image.asset("assets/2.PNG"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                onPressed: () {
+                  Navigator.pushNamed(context, wwelcome.id);
+                },
+                child: Text("Go to home page")),
+          ),
+        ]),
+      )),
+    );
+  }
+}
